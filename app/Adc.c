fdf0d175c01258944222266b492eea8676c4a3f4
EmeraldWolfram <stevenfgy@yahoo.com>
2015-11-22 16:57:06 +0800

    Added functions and required definition to Adc.h 1. Added configureADC function to awake ADCx 2. Added set and get functions to set or get value

--- app/Adc.c
+++ app/Adc.c
@@ -1,4 +1,91 @@
 #include "Adc.h"
 #include "stm32f4xx_hal_rcc.h"
 
+/**
+ * configureADC:
+ *
+ * This function enable the ADCx by awaken the ADCx and enable the CLOCK
+ *
+ * NOTE: It call adcUnresetEnableClock in Rcc.h to enable the CLOCK
+ *
+ * @aDCx		is the selection of ADC (ADC1, ADC2 or ADC3)
+ */
+void configureADC(ADC* aDCx){
+	adcUnresetEnableClock(aDCx);
+	aDCx->CR2 |= AWAKEN_ADC;
+	aDCx->CR1 |= EOC_INTERRUPT_ENB;
+}
 
+/**
+ * setResolution:
+ *
+ * This function configure the resolution of the converted analogue signal.
+ * Define how precise the result wanted.
+ *
+ * NOTE: The resolution set for every channels configured for ADCx
+ *
+ * @aDCx	is the selection of ADC (ADC1, ADC2 or ADC3)
+ * @res		is the resolution of the result, (6, 8, 10 or 12)bits
+ */
+void setResolution(int res, ADC* aDCx){
+	aDCx->CR1 &= ~(3 << 24);	//MASK RESOLUTION control bits
+	aDCx->CR1 |= res << 24;
+}
+
+/**
+ * setSampleTime:
+ *
+ * This function select the sample time for conversion of each channel.
+ * Define the time to charge up the capacitor and stabilise the voltage
+ *
+ * @aDCx		is the selection of ADC (ADC1, ADC2 or ADC3)
+ * @sampTime	is the resolution of the result, (6, 8, 10 or 12)bits
+ */
+void setSampleTime(SampleTime sampTime, ADC* aDCx, int channel){
+	if(channel < 10){
+		aDCx->SMPR2 &= ~(7 << channel);	//MASK SAMPLING TIME control bits
+		aDCx->SMPR2 |= (sampTime << channel*3);
+	}
+	else {
+		aDCx->SMPR1 &= ~(7 << channel);
+		aDCx->SMPR1 |= (sampTime << (channel - 10)*3);
+	}
+}
+
+/**
+ * getRegularData:
+ *
+ * This function read the data of regular group and return it to the caller
+ *
+ * @aDCx		is the selection of ADC (ADC1, ADC2 or ADC3)
+ */
+int getRegularData(ADC* aDCx){
+	return aDCx->DR;
+}
+
+/**
+ * getInjectedData:
+ *
+ * This function read the data of injected group and return it to the caller
+ *
+ * @aDCx		is the selection of ADC (ADC1, ADC2 or ADC3)
+ * @queue		is the current position in the sequence of queue in the list
+ */
+int getInjectedData(ADC* aDCx, int queue){
+	switch(queue)
+	case 0:
+		return aDCx->JDR1;
+		break;
+	case 1:
+		return aDCx->JDR2;
+		break;
+	case 3:
+		return aDCx->JDR3;
+		break;
+	case 4:
+		return aDCx->JDR4;
+		break;
+	default:
+		return aDCx->JDR1;
+		break;
+}
--- app/Adc.h
+++ app/Adc.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 typedef struct ADC_t ADC;
+#include "Rcc.h"
 struct ADC_t{
 	uint32_t  SR;
 	uint32_t  CR1;
@@ -29,7 +30,7 @@ struct ADC_t{
 typedef struct CommonADC_t CommonADC;
 struct CommonADC_t{
   uint32_t CSR;
-  uint32_t CDR;
+  uint32_t CCR;
   uint32_t CDR;
 };
 
@@ -37,5 +38,45 @@ struct CommonADC_t{
 #define ADC1        ((ADC*)0x40012000)
 #define ADC2        ((ADC*)0x40012100)
 #define ADC3        ((ADC*)0x40012200)
+
+#define AWAKEN_ADC	1
+
+#define START_REGULAR_CONVERSION	(1 << 30)
+#define START_INJECT_CONVERSION		(1 << 22)
+
+#define EOC_INTERRUPT_ENB		(1 << 5)
+#define JEOC_INTERRUPT_ENB		(1 << 7)
+//********************************************
+#define ENABLE_WATCHDOG			(1 << 23)
+#define ENABLE_JWATCHDOG		(1 << 22)
+//Mode (Single Mode or ContinuousMode)********
+#define CONTINUOUS_CONVERSION	(1 << 1)
+//Resolution**********************************
+#define RESOLUTION_12_BITS	0
+#define RESOLUTION_10_BITS	1
+#define RESOLUTION_8_BITS	2
+#define RESOLUTION_6_BITS	3
+
+typedef enum{
+	CYCLE_3,
+	CYCLE_15,
+	CYCLE_28,
+	CYCLE_56,
+	CYCLE_84,
+	CYCLE_112,
+	CYCLE_114,
+	CYCLE_480
+}SampleTime;
+
+void configureADC(ADC* aDCx);
+
+void setResolution(int res, ADC* aDCx);
+void setSampleTime(SampleTime sampTime, ADC* aDCx, int channel);
+
+//void setRegularQueue(int numOfChannel);	//SET QUEUE (TO DO)
+
+int getRegularData(ADC* aDCx);
+int getInjectedData(ADC* aDCx, int queue);
+
 
 #endif	//__Adc_H__
